2a7677eba68ef06eb5c9281bf101228b9a657508
Luci-Paz <188948837+Luci-Paz@users.noreply.github.com>
2025-01-26 13:39:46 -0500

    Initial commit, added .gitignore and project files

---
+++ RayTracingProject/RayTracingProject.cpp
@@ -0,0 +1,34 @@
+// RayTracingProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    //image
+
+    int imageWidth = 256;
+    int imageHeight = 256;
+
+
+    //render
+
+    std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
+
+    for (int i = 0; i < imageHeight; i++) {
+        for (int j = 0; j < imageWidth; j++) {
+            auto r = double(j) / (imageWidth - 1);
+            auto g = double(i) / (imageHeight - 1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+
+    }
+
+}
+
